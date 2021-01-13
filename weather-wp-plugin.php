2355a464a6d4a62b64d3c6ad6701f2a35509c7f8
vijayakunwar <kunwar.vijaya@gmail.com>
2021-01-13 16:53:32 +1000

    added plugin detail information  and  weather-wp-plugin.php fiel

---
+++ weather-wp-plugin.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name:       Weather plugin for CP5634 by jc612951
+ * Plugin URI:        https://vijayakunwar.github.io/
+ * Description:       Plugin to show current weather status that will be placed as widget
+ * Version:           1.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Vijaya Kunwar
+ * Author URI:        https://vijayakunwar.github.io/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ *
+ */
+
+echo "hello world";
